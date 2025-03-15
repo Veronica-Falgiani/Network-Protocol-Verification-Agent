6a1efbe4b6f21262ca3ea013675cddf29478b1c8
Veronica-Falgiani <veronica03f@gmail.com>
2025-03-15 11:45:21 +0100

    Port parse working

--- src/port_scan.py
+++ src/port_scan.py
@@ -2,6 +2,44 @@ import socket
 from scapy.all import *
 
 
+# Parsing ports we need to scan from user input
+def port_parse(port_str: str):
+    ports = []
+
+    # Contiguous port list
+    if ":" in port_str:
+        p_range = port_str.split(":")
+        if (
+            p_range[0].isnumeric()
+            and p_range[1].isnumeric()
+            and 0 <= int(p_range[0]) <= 65535
+            and 0 <= int(p_range[1]) <= 65535
+        ):
+            for i in range(int(p_range[0]), int(p_range[1]) + 1):
+                ports.append(i)
+        else:
+            print("Error with input ports")
+            # Random port list
+    elif "," in port_str:
+        p_list = port_str.split(",")
+        for item in p_list:
+            if item.isnumeric() and int(item) >= 0 and int(item) <= 65535:
+                ports.append(int(item))
+            else:
+                print(f"port {item} not valid! Skipping it")
+
+    # Single port
+    elif port_str.isnumeric() and int(port_str) >= 0 and int(port_str) <= 65535:
+        ports.append(int(port_str))
+
+    # Generic error
+    else:
+        print("Error with input ports")
+        sys.exit()
+
+    return ports
+
+
 # Send: connect() (TCP with SYN)
 # Rec:  TCP with SYN/ACK -> open
 #       no response -> closed/filtered
@@ -13,21 +51,32 @@ def tcp_connect_scan(ip: str, port: int):
         s.close()
         print("Port: " + str(port) + " open")
     except socket.error:
-        print("Port: " + str(port) + " closed/filtered")
+        pass
+        # print("Port: " + str(port) + " closed/filtered")
 
 
 # Send: SYN
 # Rec:  SYN/ACK -> RST -> open
 #       RST -> closed
 #       no response/ICMP unreachable -> filtered
-def syn_scan(ip: str):
-    pass
+def tcp_syn_scan(ip: str, port: int):
+    packet = IP(dst=ip) / TCP(dport=port, flags="S")
+    res = sr1(packet, timeout=5, verbose=0)
+    flag_res = res.sprintf("%TCP.flags%")
+
+    if flag_res == "RA":
+        pass
+        # print(f"{port} \t closed")
+    elif flag_res == "SA":
+        print(f"{port} \t open")
+    else:
+        print(f"{port} \t filtered")
 
 
 # Send: ACK
 # Res:  no response after tumeout/ICMP unreachable error -> filtered
 #       RST -> unfiltered
-def ack_scan(ip: str):
+def tcp_ack_scan(ip: str):
     pass
 
 
@@ -35,7 +84,7 @@ def ack_scan(ip: str):
 # Res:  RST with non-zero window field -> open
 #       RST with zero window field -> closed
 #       no response/ICMP unreachable -> filtered
-def window_scan(ip: str):
+def tcp_window_scan(ip: str):
     pass
 
 
@@ -43,7 +92,7 @@ def window_scan(ip: str):
 # # Rec:  no repsonse: open/filtered
 #       TCP RST -> closed
 #       ICMP UNREACHABLE -> filtered
-def fin_scan(ip: str):
+def tcp_fin_scan(ip: str):
     pass
 
 
@@ -51,7 +100,7 @@ def fin_scan(ip: str):
 # Rec:  no repsonse: open/filtered
 #       TCP RST -> closed
 #       ICMP UNREACHABLE -> filtered
-def null_scan(ip: str):
+def tcp_null_scan(ip: str):
     pass
 
 
@@ -59,7 +108,7 @@ def null_scan(ip: str):
 # Rec:  no repsonse: open/filtered
 #       TCP RST -> closed
 #       ICMP UNREACHABLE -> filtered
-def xmas_scan(ip: str):
+def tcp_xmas_scan(ip: str):
     pass
 
 
