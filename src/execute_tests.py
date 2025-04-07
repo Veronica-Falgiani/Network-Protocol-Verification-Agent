918ef2195272fbe98d417db5d6fb31ae446e1694
Veronica-Falgiani <veronica03f@gmail.com>
2025-04-07 14:40:42 +0200

    Basic json test parsing

--- src/execute_tests.py
+++ src/execute_tests.py
@@ -1,13 +1,40 @@
 import json
 import os
+from terminal_colors import print_warning, print_fail, print_ok
 
 base_dir = os.path.dirname(__file__)
 
 
-def test(services: dict):
+def print_test(services: dict):
+    print("PORT \t PROTOCOL")
+
     for port, prot in services.items():
+        print(f"\n{port} \t {prot}")
         rel_path = "tests/" + prot.lower() + "_test.json"
         path = os.path.join(base_dir, rel_path)
+
         with open(path) as file:
-            info = json.load(file)
-            print(info["tests"])
+            test_file = json.load(file)
+            tests = test_file["tests"]
+
+            for name, info in tests.items():
+                if "send" in info:
+                    test(name, info)
+                else:
+                    print(f"|\\_ {name}")
+                    print(f"|   severity: {info['severity']}")
+                    print(f"|")
+
+
+def test(name, info):
+    send = info["send"]
+    recv = info["recv"]
+    res = "20"
+    print(send, recv)
+
+    # Send and receive packages
+
+    if recv == res:
+        print(f"|\\_ {name}")
+        print(f"|   severity: {info['severity']}")
+        print(f"|")
