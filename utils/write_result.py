8118079614f2d34713df004197c95bad29771bb4
Veronica-Falgiani <veronica03f@gmail.com>
2025-04-22 11:31:58 +0200

    Updated print to txt

--- utils/write_result.py
+++ utils/write_result.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import os
 
 
-def result(found_ports: dict, services: dict, report: dict):
+def txt_result(found_ports: dict, services: dict, report: list):
     # Creates a directory (if it doesn't exist) and a result file
     file_name = "res/results_" + datetime.today().strftime("%Y-%m-%d_%H:%M:%S") + ".txt"
     os.makedirs(os.path.dirname(file_name), exist_ok=True)
@@ -10,24 +10,21 @@ def result(found_ports: dict, services: dict, report: dict):
 
     with open(file_name, "w") as res_file:
         res_file.write("##### RESULTS #####\n\n")
-        res_file.write("PORT \t STATUS \t SERVICE\n")
+        res_file.write("PORT \t PROTOCOL \t SERVICE\n")
         res_file.write("----------------------------\n")
 
-        for port, status in found_ports.items():
-            if services[port] != "undefined":
-                res_file.write(f"\n{str(port)} \t {status} \t {services[port]}\n")
+        for result in report:
+            res_file.write(f"\n{result.port} \t {result.prot} \t {result.service}\n")
 
-                # writes a line for every test that has found a vulnerability in the protocol
-                if port in report.keys():
-                    results = report[port]
+            # No test found for the protocol
+            if len(result.vulns) == 0:
+                res_file.write("|\\_ --- NO TESTS FOUND FOR THIS PROTOCOL ---\n")
 
-                    for name, info in results.items():
-                        res_file.write(f"|\\_ {name}\n")
-                        res_file.write(f"|   description: {info['description']}\n")
-                        res_file.write(f"|   severity: {info['severity']}\n")
-
-                # No test found for the protocol
-                else:
-                    res_file.write("|\\_ --- NO TESTS FOUND FOR THIS PROTOCOL ---\n")
+            # writes a line for every test that has found a vulnerability in the protocol
+            else:
+                for vuln in result.vulns:
+                    res_file.write(f"|\\_ {vuln['name']}\n")
+                    res_file.write(f"|   description: {vuln['description']}\n")
+                    res_file.write(f"|   severity: {vuln['severity']}\n")
 
     print(f"\nResults can be found in: {file_name}")
