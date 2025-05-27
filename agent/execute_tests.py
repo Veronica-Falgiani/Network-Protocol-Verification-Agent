5704d1b98205ce592f458236cb0f3d2ddd51e726
Veronica-Falgiani <veronica03f@gmail.com>
2025-05-27 11:14:21 +0200

    Fixed login prompt

--- agent/execute_tests.py
+++ agent/execute_tests.py
@@ -95,8 +95,6 @@ class ExecuteTests:
                     # Asks the user for login info and inserts the correct login messages in a list
                     login_list = self.try_login(prot, port, service, login)
 
-                    print(auth_misconfigs)
-
                     # Start testing for misconfigurations
                     if login_list:
                         print("Ciao")
@@ -137,7 +135,7 @@ class ExecuteTests:
                             print("\033[K", end="\r")
 
             except FileNotFoundError:
-                results = Results(port, prot, service, 0)
+                results = Results(port, prot, service, 0, 0)
 
             self.report.append(results)
 
@@ -249,15 +247,6 @@ class ExecuteTests:
     def check_tls(self, service: str, results: Results):
         if "TLSv1.3" not in service and "TLSv1.2" not in service:
             results.unsafe_tls = True
-
-    def try_login2(self, sock, login):
-        login_str = login["send_str"].split("~~")
-        for string in login_str:
-            sock.send(string.encode())
-            res = sock.recv(1024)
-            if login["recv_str"] not in res.decode():
-                print("Errore login")
-            # TODO: VERIFY LOGIN SUCCESSFUL
 
     def try_login(self, prot, port, service, login) -> list:
         # Asks the user max 3 times for the password
